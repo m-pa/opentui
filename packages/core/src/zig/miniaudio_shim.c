4dde7af1e7df2ee4c734cab2bfe55183cdc2f58c
m-pa <onen7701@gmail.com>
2026-04-17 10:21:55 +0200

    enable specific audio backends

--- packages/core/src/zig/miniaudio_shim.c
+++ packages/core/src/zig/miniaudio_shim.c
@@ -1,38 +1,21 @@
 #define MINIAUDIO_IMPLEMENTATION
 
 #if defined(__linux__)
+#define MA_ENABLE_ONLY_SPECIFIC_BACKENDS
 #define MA_ENABLE_ALSA
-#define MA_NO_PULSEAUDIO
-#define MA_NO_JACK
-#define MA_NO_OSS
-#define MA_NO_AAUDIO
-#define MA_NO_OPENSL
-#define MA_NO_COREAUDIO
-#define MA_NO_WASAPI
-#define MA_NO_DSOUND
-#define MA_NO_WINMM
+#define MA_ENABLE_PULSEAUDIO
 #endif
 
 #if defined(__APPLE__)
+#define MA_ENABLE_ONLY_SPECIFIC_BACKENDS
 #define MA_ENABLE_COREAUDIO
-#define MA_NO_PULSEAUDIO
-#define MA_NO_ALSA
-#define MA_NO_JACK
-#define MA_NO_OSS
-#define MA_NO_WASAPI
-#define MA_NO_DSOUND
-#define MA_NO_WINMM
 #endif
 
 #if defined(_WIN32)
+#define MA_ENABLE_ONLY_SPECIFIC_BACKENDS
 #define MA_ENABLE_WASAPI
-#define MA_NO_DSOUND
-#define MA_NO_WINMM
-#define MA_NO_PULSEAUDIO
-#define MA_NO_ALSA
-#define MA_NO_JACK
-#define MA_NO_OSS
-#define MA_NO_COREAUDIO
+#define MA_ENABLE_DSOUND
+#define MA_ENABLE_WINMM
 #endif
 
 #include "vendor/miniaudio/miniaudio.h"
